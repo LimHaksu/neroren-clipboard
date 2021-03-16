88e6e5e4874ad0222c30946d37380b3edf3044ab
Lim Haksu <neroren2@gmail.com>
2021-03-16 21:37:35 +0900

    feat: google spread sheet copy logic added
    * google docs, presentation not supproted now

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,4 +1,42 @@
-document.addEventListener("copy", () => {
-    const selection = document.getSelection();
-    chrome.runtime.sendMessage({ type: "copy", data: { selectionText: selection ? selection.toString() : "" } });
-});
+document.addEventListener(
+    "copy",
+    () => {
+        lazyCopy();
+    },
+    {
+        capture: true,
+    }
+);
+document.addEventListener(
+    "cut",
+    () => {
+        lazyCopy();
+    },
+    {
+        capture: true,
+    }
+);
+
+const lazyCopy = () => {
+    setTimeout(async () => {
+        let selection = document.getSelection()?.toString().trim();
+
+        // for google spread sheet
+        if (!selection) {
+            const tempDom = document.createElement("textarea");
+            tempDom.style.position = "absolute";
+            tempDom.style.top = `${window.innerHeight / 2}px`;
+            tempDom.style.zIndex = "-9999";
+            document.body.appendChild(tempDom);
+            tempDom.focus();
+            document.execCommand("paste");
+            selection = tempDom.value;
+            tempDom.remove();
+        }
+
+        chrome.runtime.sendMessage({
+            type: "copy",
+            data: { selectionText: selection },
+        });
+    }, 0);
+};
